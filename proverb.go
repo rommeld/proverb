a62a07108062c3a2e0eb71db069b28ea14ea842f
rommeld <rommel.denny@gmail.com>
2020-12-03 00:51:08 +0100

    finished the algo.

--- proverb.go
+++ proverb.go
@@ -1,15 +1,21 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
 
-// Proverb should have a comment documenting it.
+import (
+	"fmt"
+)
+
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+
+	var output []string
+
+	for count, str := range rhyme {
+		var verb string
+		if count > len(rhyme)-2 {
+			verb = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+		} else {
+			verb = fmt.Sprintf("For want of a %s the %s was lost.", str, rhyme[count+1])
+		}
+		output = append(output, verb)
+	}
+	return output
 }
